ed2afd57ce6f28ffc4fb5d9a6d966789ab79629a
Nikhil NAvin <nnavin@akamai.com>
2023-01-13 18:20:45 +0530

    reset

--- rust-ds/src/lib.rs
+++ rust-ds/src/lib.rs
@@ -2,14 +2,6 @@
 mod tests {
     #[test]
     fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+        assert_eq!(true, false);
     }
 }
-
-pub fn welcome_message() -> &'static str{
-    "Hello Welcone to Rust DataStructure lessons"
-}
-
-pub mod stack;
-pub mod cqueue;
